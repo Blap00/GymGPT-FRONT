428cd449bd80ae74a7cc0cee1795c3cceba81ef6
MoonLight-coder <angelogonzacorre111@gmail.com>
2024-09-12 09:56:50 -0300

    iniciar Sesion Creado
    Creado el inicio de Sesion

--- Frontend-IONIC/src/app/iniciar-sesion/iniciar-sesion.page.ts
+++ Frontend-IONIC/src/app/iniciar-sesion/iniciar-sesion.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-iniciar-sesion',
   templateUrl: './iniciar-sesion.page.html',
   styleUrls: ['./iniciar-sesion.page.scss'],
 })
-export class IniciarSesionPage implements OnInit {
+export class IniciarSesionPage {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  goToIniciarSesion() {
+    this.navCtrl.navigateForward('/iniciar-sesion')
   }
 
+  goToRegistrarse() {
+    this.navCtrl.navigateForward('/registrarse')
+  }
+  goToHome(){
+    this.navCtrl.navigateForward('/home')
+  }
 }
